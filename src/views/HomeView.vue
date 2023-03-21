a764e0206ec9ffdbc25521fa9f2ff48a4c922d3e
mimix09 <mpianka253@gmail.com>
2023-03-21 19:41:14 +0100

    dodanie metody find

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,47 @@
-<script setup>
+<script>
 import HeaderPage from '@/components/HeaderPage.vue'
 import szpej from '@/data/szpej.json'
+import { findDir } from '@vue/compiler-core';
 const zmienna = szpej;
 console.log(zmienna);
+
+  export default{
+    data(){
+      return{
+        wybieranie: 'wybierz'
+      }
+    },
+
+    methods: {
+      formSumbit(){
+        zmienna.forEach(element => {
+          if(element.category.find(c => c == this.wybieranie) == this.wybieranie){
+            console.log(element.name);
+          }
+        });
+      }
+    }
+  };
+
+
 </script>
 
 <template>
   <HeaderPage />
   <main>
     <h1>Home Page</h1>
-    <div></div>
+
+    <form v-on:submit.prevent="formSumbit">
+      <select v-model="wybieranie">
+        <option disabled value="">Wybierz</option>
+        <option>mountains</option>
+        <option>bicycle</option>
+        <option>skitour</option>
+      </select>
+
+      <dir>{{ wybieranie }}</dir>
+
+      <button type="submit">Wyślij</button>
+    </form>
   </main>
 </template>
